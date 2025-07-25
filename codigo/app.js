1df791f1ffc1e950008aec1be2f46431e3af5a8a
fray osorio <frayosorio@gmail.com>
2025-07-25 11:29:08 -0500

    Se agrega clase TABLA

--- codigo/app.js
+++ codigo/app.js
@@ -3,9 +3,17 @@ angular.module("JuegoBingo", [])
         function ($scope) {
 
             $scope.cantor = new Cantor();
+            $scope.tablas = [];
 
             $scope.iniciarJuego = function () {
-
+                const totalJugadores = 7;
+                for (let i = 0; i < totalJugadores; i++) {
+                    $scope.tablas.push({
+                        nombre: "Jugador " + (i + 1),
+                        tabla: new Tabla(),
+                        ganador: false
+                    });
+                }
             }
 
             $scope.sacarBalota = function () {
--- codigo/tabla.js
+++ codigo/tabla.js
@@ -4,7 +4,10 @@ function Tabla() {
     for (var f = 0; f < 5; f++) {
         this.tabla[f] = [];
         for (var c = 0; c < 5; c++) {
-            this.tabla[f][c] = ?;
+            //solo cuando no sea la posicion central
+            if (!(c == 2 && f == 2)) {
+                this.tabla[f][c] = c * 15 + Math.floor(Math.random() * 15) + 1;
+            }
         }
     }
 }
