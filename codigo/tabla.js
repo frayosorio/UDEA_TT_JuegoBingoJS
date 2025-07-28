1ca161a1ac8802f3be007e351ecbb82c3604f94e
fray osorio <frayosorio@gmail.com>
2025-07-28 11:47:43 -0500

    Se agrega funcion que verifica si hubo bingo

--- codigo/tabla.js
+++ codigo/tabla.js
@@ -9,11 +9,12 @@ function Tabla() {
             //solo cuando no sea la posicion central
             if (!(c == 2 && f == 2)) {
                 this.tabla[f][c] = c * 15 + Math.floor(Math.random() * 15) + 1;
+                this.tapada[f][c] = false;
             }
             else {
-                this.tabla[f][c] = 0;
+                this.tabla[f][c] = "LIBRE";
+                this.tapada[f][c] = true;
             }
-            this.tapada[f][c] = false;
         }
     }
 
@@ -25,5 +26,53 @@ function Tabla() {
                 }
             }
         }
+    }
+
+    this.verificarBingo = function () {
+        // Verificar Filas
+        for (let f = 0; f < 5; f++) {
+            let bingoFila = true;
+            for (let c = 0; c < 5; c++) {
+                if (!this.tapadas[f][c]) {
+                    bingoFila = false;
+                    break;
+                }
+            }
+            if (bingoFila) return true;
+        }
+
+        // Verificar Columnas
+        for (let c = 0; c < 5; c++) {
+            let bingoColumna = true;
+            for (let f = 0; f < 5; f++) {
+                if (!this.tapadas[f][c]) {
+                    bingoColumna = false;
+                    break;
+                }
+            }
+            if (bingoColumna) return true;
+        }
+
+        // Verificar Diagonal Principal (arriba-izquierda a abajo-derecha)
+        let bingoDiagonal1 = true;
+        for (let i = 0; i < 5; i++) {
+            if (!this.tapadas[i][i]) { 
+                bingoDiagonal1 = false;
+                break;
+            }
+        }
+        if (bingoDiagonal1) return true;
+
+        // Verificar Diagonal secundaria (arriba-derecha a abajo-izquierda)
+        let bingoDiagonal2 = true;
+        for (let i = 0; i < 5; i++) {
+            if (!this.tapadas[i][4-i]) { 
+                bingoDiagonal2 = false;
+                break;
+            }
+        }
+        if (bingoDiagonal2) return true;
+
+        return false;
     }
 }
